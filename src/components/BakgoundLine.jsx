431c0a59d3f83f50df429f1367dcfda9cc29ad99
Juan Oliver <juanoliver995@gmail.com>
2022-10-18 21:38:30 -0300

    cambios en background

---
+++ src/components/BakgoundLine.jsx
@@ -0,0 +1,13 @@
+
+
+const BakgoundLine = () => {
+    return (
+        <>
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+        </>
+    )
+}
+
+export default BakgoundLine
--- src/components/LineUp.jsx
+++ src/components/LineUp.jsx
@@ -6,53 +6,51 @@ import 'swiper/css';
 import { Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/autoplay';
-
+import BakgoundLine from './BakgoundLine';
 const LineUp = () => {
     return (
-        <div id='line'>
-            <div className='bg-line'>
-                <Swiper
-                    id='description'
-                    modules={[Autoplay]}
-                    slidesPerView={1}
-                    spaceBetween={500}
-                    centeredSlidesBounds={true}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: false
-                    }}
-                >
-                    <SwiperSlide>
-                        <div className='lineup'>
-                            <h1>ARTISTA</h1>
-                            <h1>PRINCIPAL</h1>
-                            <div className='container-img-lineup'>
-                                <img src={Lgante} />
-                            </div>
+        <div id='line' className='line'>
+            <Swiper
+                id='description'
+                modules={[Autoplay]}
+                slidesPerView={1}
+                spaceBetween={500}
+                centeredSlidesBounds={true}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false
+                }}
+            >
+                <SwiperSlide>
+                    <div className='lineup'>
+                        <h1>ARTISTA</h1>
+                        <h1>PRINCIPAL</h1>
+                        <div className='container-img-lineup'>
+                            <img src={Lgante} />
                         </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='lineup'>
-                            <h1>DJ</h1>
-                            <h1>INVITADO</h1>
-                            <div className='container-img-lineup'>
-                                <img src={Nahu} />
-                            </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='lineup'>
+                        <h1>DJ</h1>
+                        <h1>INVITADO</h1>
+                        <div className='container-img-lineup'>
+                            <img src={Nahu} />
                         </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='lineup'>
-                            <h1>BANDA</h1>
-                            <h1>INVITADA</h1>
-                            <div className='container-img-lineup'>
-                                <img src={Lgante} />
-                            </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='lineup'>
+                        <h1>BANDA</h1>
+                        <h1>INVITADA</h1>
+                        <div className='container-img-lineup'>
+                            <img src={Lgante} />
                         </div>
-                    </SwiperSlide>
-
-                </Swiper>
-            </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            <BakgoundLine />
         </div>
     )
 }
