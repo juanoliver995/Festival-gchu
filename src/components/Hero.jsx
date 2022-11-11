5ea968dee4d08513cd35bd2597acd55efdba516f
Juan Martin Oliver <95033445+juanoliver995@users.noreply.github.com>
2022-11-11 17:46:43 -0300

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,9 @@
-import LogoFestival from "../assets/LOGO GCHU FEST.png"
+import videoHero from "../assets/LGANTEFRAN2.mp4"
 
 const Hero = () => {
     return (
         <div className="container-hero" id="hero">
-            <div className="container-texto">
-                <h1>Gualeguaychu Festival</h1>
-                <div className="lista">
-                    <div className="item">
-                        <p>SABADO 19 DE NOVIEMBRE</p>
-                    </div>
-                    <div className="item">
-                        <p>CORSODROMO GUALEGUAYCHU</p>
-                    </div>
-                    <div className="item">
-                        <p>SHOW L-GANTE</p>
-                    </div>
-                    <div className="item">
-                        <p>BANDAS, DJS Y ARTISTAS EN VIVO</p>
-                    </div>
-
-                </div>
-            </div>
-            <div className="container-hero-img">
-                <img src={LogoFestival} />
-            </div>
+            <video src={videoHero} muted={true} loop={true} autoPlay={true}></video>
         </div>
     )
 }
---
+++ src/components/ReyMomo.jsx
@@ -0,0 +1,13 @@
+import reymomo from "../assets/REY MOMO web.png"
+
+const ReyMomo = () => {
+    return (
+        <div className="rey-momo">
+            <h1>Nuevo Sector</h1>
+            <p>Los esperamo para disfrutar una noche inolvidable</p>
+            <img src={reymomo} />
+        </div>
+    )
+}
+
+export default ReyMomo
